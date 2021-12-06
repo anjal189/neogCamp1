0bf4ae1b80616a18ebc236538bcd45f266dc24dc
anjali98Rahane <61266918+anjal189@users.noreply.github.com>
2021-12-06 04:21:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//console.log("Anjali Rahane")
+// var readlineSync = require ('readline-sync')
+// var uName = readlineSync.question('what is your name ?')
+// console.log(uName)
+var readlineSync = require ('readline-sync')
+var uName = readlineSync.question('what is your name ?')
+var msgForUser="WELCOME " + uName;
+console.log(msgForUser)
